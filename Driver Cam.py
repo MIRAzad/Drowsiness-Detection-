0ea77922ddc0adbd7bf628a0997e000a3a75ea4d
unknown <mirmusaib99@gmail.com>
2020-07-16 20:38:33 +0530

    first file to git

---
+++ Driver Cam.py
@@ -0,0 +1,273 @@
+from __future__ import division
+import numpy
+from pygame import mixer
+import time
+import cv2
+from tkinter import *
+import tkinter.messagebox
+
+import dlib
+from imutils import face_utils
+import cv2
+import numpy as np
+from scipy.spatial import distance as dist
+
+
+
+root=Tk()
+root.geometry('500x570')
+frame = Frame(root, relief=RIDGE, borderwidth=2)
+frame.pack(fill=BOTH,expand=1)
+root.title('Driver Cam')
+frame.config(background='light blue')
+label = Label(frame, text="Driver Cam",bg='light blue',font=('Times 35 bold'))
+label.pack(side=TOP)
+filename = PhotoImage(file="C:/Users/Mir/Desktop/Spyder tuto/Drowsiness-monitoring/demo.png")
+background_label = Label(frame,image=filename)
+background_label.pack(side=TOP)
+
+
+
+def hel():
+   help(cv2)
+
+def Contri():
+   tkinter.messagebox.showinfo("Contributors","\n1.Mayur Kadam\n2. Abhishek Ezhava \n3. Rajendra Patil \n")
+
+
+def anotherWin():
+   tkinter.messagebox.showinfo("About",'Driver Cam version v1.0\n Made Using\n-OpenCV\n-Numpy\n-Tkinter\n In Python 3')
+                                    
+   
+
+menu = Menu(root)
+root.config(menu=menu)
+
+subm1 = Menu(menu)
+menu.add_cascade(label="Tools",menu=subm1)
+subm1.add_command(label="Open CV Docs",command=hel)
+
+subm2 = Menu(menu)
+menu.add_cascade(label="About",menu=subm2)
+subm2.add_command(label="Driver Cam",command=anotherWin)
+subm2.add_command(label="Contributors",command=Contri)
+
+
+
+def exitt():
+   exit()
+
+  
+def web():
+   capture =cv2.VideoCapture(0)
+   while True:
+      ret,frame=capture.read()
+      gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+      cv2.imshow('frame',frame)
+      if cv2.waitKey(1) & 0xFF ==ord('q'):
+         break
+   capture.release()
+   cv2.destroyAllWindows()
+
+def webrec():
+   capture =cv2.VideoCapture(0)
+   fourcc=cv2.VideoWriter_fourcc(*'XVID') 
+   op=cv2.VideoWriter('Sample1.avi',fourcc,11.0,(640,480))
+   while True:
+      ret,frame=capture.read()
+      gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+      cv2.imshow('frame',frame)
+      op.write(frame)
+      if cv2.waitKey(1) & 0xFF ==ord('q'):
+         break
+   op.release()
+   capture.release()
+   cv2.destroyAllWindows()   
+
+def webdet():
+   capture =cv2.VideoCapture(0)
+   face_cascade = cv2.CascadeClassifier("C:/Users/Mir/Desktop/Spyder tuto/Drowsiness-monitoring/lbpcascade_frontalface.xml")
+   eye_glass = cv2.CascadeClassifier("C:/Users/Mir/Desktop/Spyder tuto/Drowsiness-monitoring/haarcascade_eye_tree_eyeglasses.xml")
+   
+
+   while True:
+       ret, frame = capture.read()
+       gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+       faces = face_cascade.detectMultiScale(gray)
+    
+
+       for (x,y,w,h) in faces:
+           font = cv2.FONT_HERSHEY_COMPLEX
+           cv2.putText(frame,'Face',(x+w,y+h),font,1,(250,250,250),2,cv2.LINE_AA)
+           cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+           roi_gray = gray[y:y+h, x:x+w]
+           roi_color = frame[y:y+h, x:x+w]
+        
+          
+           eye_g = eye_glass.detectMultiScale(roi_gray)
+           for (ex,ey,ew,eh) in eye_g:
+              cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+       
+       cv2.imshow('frame',frame)
+       if cv2.waitKey(1) & 0xff == ord('q'):
+          break
+   capture.release()
+   cv2.destroyAllWindows()
+def webdetRec():
+   capture =cv2.VideoCapture(0)
+   face_cascade = cv2.CascadeClassifier("C:/Users/Mir/Desktop/Spyder tuto/Drowsiness-monitoring/lbpcascade_frontalface.xml")
+   eye_glass = cv2.CascadeClassifier("C:/Users/Mir/Desktop/Spyder tuto/Drowsiness-monitoring/haarcascade_eye_tree_eyeglasses.xml")
+   fourcc=cv2.VideoWriter_fourcc(*"XVID") 
+   op=cv2.VideoWriter("Sample2.avi",fourcc,9.0,(640,480))
+
+   while True:
+       ret, frame = capture.read()
+       gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+       faces = face_cascade.detectMultiScale(gray)
+    
+
+       for (x,y,w,h) in faces:
+           font = cv2.FONT_HERSHEY_COMPLEX
+           cv2.putText(frame,'Face',(x+w,y+h),font,1,(250,250,250),2,cv2.LINE_AA)
+           cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+           roi_gray = gray[y:y+h, x:x+w]
+           roi_color = frame[y:y+h, x:x+w]
+        
+          
+
+           eye_g = eye_glass.detectMultiScale(roi_gray)
+           for (ex,ey,ew,eh) in eye_g:
+              cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+       op.write(frame)
+       cv2.imshow('frame',frame)
+       if cv2.waitKey(1) & 0xff == ord('q'):
+          break
+   op.release()      
+   capture.release()
+   cv2.destroyAllWindows()
+
+   
+def alert():
+   mixer.init()
+   alert=mixer.Sound("beep-07.wav")
+   alert.play()
+   time.sleep(0.1)
+   alert.play()   
+   
+def blink():
+    global ratio
+    w, h = img.shape
+    if width is None and height is None:
+        return img
+    elif width is None:
+        ratio = height / h
+        width = int(w * ratio)
+        resized = cv2.resize(img, (height, width), interpolation)
+        return resized
+    else:
+        ratio = width / w
+        height = int(h * ratio)
+        resized = cv2.resize(img, (height, width), interpolation)
+        return resized
+######
+def shape_to_np(shape, dtype="int"):
+    coords = np.zeros((68, 2), dtype=dtype)
+    for i in range(36,48):
+        coords[i] = (shape.part(i).x, shape.part(i).y)
+    return coords
+def eye_aspect_ratio(eye):
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+ 
+	# compute the euclidean distance between the horizontal
+	# eye landmark (x, y)-coordinates
+    C = dist.euclidean(eye[0], eye[3])
+    print (A,B,C)
+	# compute the eye aspect ratio
+    ear = (A + B) / (2.0 * C)
+ 
+	# return the eye aspect ratio
+    return ear
+camera = cv2.VideoCapture(0)
+
+predictor_path = 'shape_predictor_68_face_landmarks.dat_2'
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(predictor_path)
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+total=0
+while True:
+    ret, frame = camera.read()
+    if ret == False:
+        print('Failed to capture frame from camera. Check camera index in cv2.VideoCapture(0) \n')
+        break
+
+    frame_grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame_resized = resize(frame_grey, width=120)
+
+# Ask the detector to find the bounding boxes of each face. The 1 in the
+# second argument indicates that we should upsample the image 1 time. This
+# will make everything bigger and allow us to detect more faces.
+    dets = detector(frame_resized, 1)
+    
+    if len(dets) > 0:
+        for k, d in enumerate(dets):
+            shape = predictor(frame_resized, d)
+            shape = shape_to_np(shape)
+            leftEye= shape[lStart:lEnd]
+            rightEye= shape[rStart:rEnd]
+            leftEAR= eye_aspect_ratio(leftEye)
+            rightEAR = eye_aspect_ratio(rightEye)
+            ear = (leftEAR + rightEAR) / 2.0
+            leftEyeHull = cv2.convexHull(leftEye)
+	       
+            rightEyeHull = cv2.convexHull(rightEye)
+            cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+            cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+            if ear>.29:
+                print (ear)
+                m=1
+                print ('o')
+                cv2.putText(frame, "Eyes Open ", (10, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            else:
+                if m==1:
+                    total+=1
+                    m=0
+                    cv2.putText(frame, "blink" ,(250, 30),cv2.FONT_HERSHEY_SIMPLEX, 1.7, (0, 0, 0), 4)
+                print (ear)
+                print ('c')
+                
+                cv2.putText(frame, "Eyes close".format(total), (10, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            cv2.putText(frame, "Total Count: {}".format(total), (410, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 255), 2)
+            for (x, y) in shape:
+                cv2.circle(frame, (int(x/ratio), int(y/ratio)), 3, (255, 255, 255), -1)
+    cv2.imshow("image", frame)
+    alert()
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        camera.release()
+        break
+   
+but1=Button(frame,padx=5,pady=5,width=39,bg='white',fg='black',relief=GROOVE,command=web,text='Open Cam',font=('helvetica 15 bold'))
+but1.place(x=5,y=104)
+
+but2=Button(frame,padx=5,pady=5,width=39,bg='white',fg='black',relief=GROOVE,command=webrec,text='Open Cam & Record',font=('helvetica 15 bold'))
+but2.place(x=5,y=176)
+
+but3=Button(frame,padx=5,pady=5,width=39,bg='white',fg='black',relief=GROOVE,command=webdet,text='Open Cam & Detect',font=('helvetica 15 bold'))
+but3.place(x=5,y=250)
+
+but4=Button(frame,padx=5,pady=5,width=39,bg='white',fg='black',relief=GROOVE,command=webdetRec,text='Detect & Record',font=('helvetica 15 bold'))
+but4.place(x=5,y=322)
+
+but5=Button(frame,padx=5,pady=5,width=39,bg='white',fg='black',relief=GROOVE,command=blink,text='Detect Eye Blink & Record With Sound',font=('helvetica 15 bold'))
+but5.place(x=5,y=400)
+
+but5=Button(frame,padx=5,pady=5,width=5,bg='white',fg='black',relief=GROOVE,text='EXIT',command=exitt,font=('helvetica 15 bold'))
+but5.place(x=210,y=478)
+
+
+root.mainloop()
+
